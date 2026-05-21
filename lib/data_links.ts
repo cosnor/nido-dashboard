21be109c9f1554a24bceac2afe680e0483136a59
Brayan De Jesus Cantillo Orozco <127346925+Brayan-CO@users.noreply.github.com>
2026-05-20 21:38:15 -0500

    predict conect to backend

---
+++ lib/data_links.ts
@@ -0,0 +1,603 @@
+export interface Bird {
+  scientificName: string;
+  name: string;
+  imageUrl: string;
+  wikipediaUrl: string;
+  family: string;
+  conservationStatus: 'LC' | 'NT' | 'VU' | 'EN' | 'CR' | 'EW' | 'EX' | 'DD';
+}
+
+export const birds: Bird[] = [
+  {
+    "scientificName": "Acropternis orthonyx",
+    "name": "Acropternis orthonyx",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Acropternis_orthonyx",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Amazona farinosa",
+    "name": "Amazona farinosa",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Amazona_farinosa",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Andigena nigrirostris",
+    "name": "Andigena nigrirostris",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Andigena_nigrirostris",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Arremon assimilis",
+    "name": "Arremon assimilis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Arremon_assimilis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Arremon brunneinucha",
+    "name": "Arremon brunneinucha",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Arremon_brunneinucha",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Atlapetes pallidinucha",
+    "name": "Atlapetes pallidinucha",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Atlapetes_pallidinucha",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Atlapetes schistaceus",
+    "name": "Atlapetes schistaceus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Atlapetes_schistaceus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Aulacorhynchus albivitta",
+    "name": "Aulacorhynchus albivitta",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Aulacorhynchus_albivitta",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Basileuterus tristriatus",
+    "name": "Basileuterus tristriatus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Basileuterus_tristriatus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Cantorchilus leucotis",
+    "name": "Cantorchilus leucotis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cantorchilus_leucotis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Catharus aurantiirostris",
+    "name": "Catharus aurantiirostris",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Catharus_aurantiirostris",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Cercomacra nigricans",
+    "name": "Cercomacra nigricans",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cercomacra_nigricans",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Cercomacroides parkeri",
+    "name": "Cercomacroides parkeri",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cercomacroides_parkeri",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Cercomacroides tyrannina",
+    "name": "Cercomacroides tyrannina",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cercomacroides_tyrannina",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Chlorospingus flavopectus",
+    "name": "Chlorospingus flavopectus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Chlorospingus_flavopectus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Cinnycerthia olivascens",
+    "name": "Cinnycerthia olivascens",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cinnycerthia_olivascens",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Cistothorus apolinari",
+    "name": "Cistothorus apolinari",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cistothorus_apolinari",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Cistothorus platensis",
+    "name": "Cistothorus platensis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cistothorus_platensis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Coereba flaveola",
+    "name": "Coereba flaveola",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Coereba_flaveola",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Colibri coruscans",
+    "name": "Colibri coruscans",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Colibri_coruscans",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Colibri cyanotus",
+    "name": "Colibri cyanotus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Colibri_cyanotus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Crotophaga ani",
+    "name": "Crotophaga ani",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Crotophaga_ani",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Crypturellus cinereus",
+    "name": "Crypturellus cinereus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Crypturellus_cinereus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Cyanocorax affinis",
+    "name": "Cyanocorax affinis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cyanocorax_affinis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Cyanocorax yncas",
+    "name": "Cyanocorax yncas",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cyanocorax_yncas",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Cyanolyca armillata",
+    "name": "Cyanolyca armillata",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cyanolyca_armillata",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Cyclarhis nigrirostris",
+    "name": "Cyclarhis nigrirostris",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cyclarhis_nigrirostris",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Cyphorhinus thoracicus",
+    "name": "Cyphorhinus thoracicus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cyphorhinus_thoracicus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Elaenia frantzii",
+    "name": "Elaenia frantzii",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Elaenia_frantzii",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Formicivora intermedia",
+    "name": "Formicivora intermedia",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Formicivora_intermedia",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Grallaria bangsi",
+    "name": "Grallaria bangsi",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Grallaria_bangsi",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Grallaria kaestneri",
+    "name": "Grallaria kaestneri",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Grallaria_kaestneri",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Grallaria nuchalis",
+    "name": "Grallaria nuchalis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Grallaria_nuchalis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Grallaria quitensis",
+    "name": "Grallaria quitensis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Grallaria_quitensis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Grallaria ruficapilla",
+    "name": "Grallaria ruficapilla",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Grallaria_ruficapilla",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Grallaricula nana",
+    "name": "Grallaricula nana",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Grallaricula_nana",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Henicorhina leucophrys",
+    "name": "Henicorhina leucophrys",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Henicorhina_leucophrys",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Henicorhina leucosticta",
+    "name": "Henicorhina leucosticta",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Henicorhina_leucosticta",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Henicorhina negreti",
+    "name": "Henicorhina negreti",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Henicorhina_negreti",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Icterus chrysater",
+    "name": "Icterus chrysater",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Icterus_chrysater",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Lophotriccus pileatus",
+    "name": "Lophotriccus pileatus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Lophotriccus_pileatus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Mecocerculus leucophrys",
+    "name": "Mecocerculus leucophrys",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Mecocerculus_leucophrys",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Metallura tyrianthina",
+    "name": "Metallura tyrianthina",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Metallura_tyrianthina",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Mimus gilvus",
+    "name": "Mimus gilvus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Mimus_gilvus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Myadestes ralloides",
+    "name": "Myadestes ralloides",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Myadestes_ralloides",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Myioborus miniatus",
+    "name": "Myioborus miniatus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Myioborus_miniatus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Myioborus ornatus",
+    "name": "Myioborus ornatus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Myioborus_ornatus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Myiothlypis coronata",
+    "name": "Myiothlypis coronata",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Myiothlypis_coronata",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Myiothlypis fulvicauda",
+    "name": "Myiothlypis fulvicauda",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Myiothlypis_fulvicauda",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Myiothlypis nigrocristata",
+    "name": "Myiothlypis nigrocristata",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Myiothlypis_nigrocristata",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Myiozetetes cayanensis",
+    "name": "Myiozetetes cayanensis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Myiozetetes_cayanensis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Nyctidromus albicollis",
+    "name": "Nyctidromus albicollis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Nyctidromus_albicollis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Ortalis columbiana",
+    "name": "Ortalis columbiana",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Ortalis_columbiana",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Percnostola rufifrons",
+    "name": "Percnostola rufifrons",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Percnostola_rufifrons",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Pheugopedius mystacalis",
+    "name": "Pheugopedius mystacalis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Pheugopedius_mystacalis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Pitangus sulphuratus",
+    "name": "Pitangus sulphuratus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Pitangus_sulphuratus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Psarocolius angustifrons",
+    "name": "Psarocolius angustifrons",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Psarocolius_angustifrons",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Psittacara wagleri",
+    "name": "Psittacara wagleri",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Psittacara_wagleri",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Pyrrhomyias cinnamomeus",
+    "name": "Pyrrhomyias cinnamomeus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Pyrrhomyias_cinnamomeus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Rupornis magnirostris",
+    "name": "Rupornis magnirostris",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Rupornis_magnirostris",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Saltator striatipectus",
+    "name": "Saltator striatipectus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Saltator_striatipectus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Scytalopus griseicollis",
+    "name": "Scytalopus griseicollis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Scytalopus_griseicollis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Scytalopus latrans",
+    "name": "Scytalopus latrans",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Scytalopus_latrans",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Scytalopus spillmanni",
+    "name": "Scytalopus spillmanni",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Scytalopus_spillmanni",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Sporophila nigricollis",
+    "name": "Sporophila nigricollis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Sporophila_nigricollis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Synallaxis albescens",
+    "name": "Synallaxis albescens",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Synallaxis_albescens",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Synallaxis azarae",
+    "name": "Synallaxis azarae",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Synallaxis_azarae",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Thamnophilus multistriatus",
+    "name": "Thamnophilus multistriatus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Thamnophilus_multistriatus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Troglodytes aedon",
+    "name": "Troglodytes aedon",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Troglodytes_aedon",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Turdus fuscater",
+    "name": "Turdus fuscater",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Turdus_fuscater",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Tyrannus melancholicus",
+    "name": "Tyrannus melancholicus",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Tyrannus_melancholicus",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Vanellus chilensis",
+    "name": "Vanellus chilensis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Vanellus_chilensis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Zimmerius chrysops",
+    "name": "Zimmerius chrysops",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Zimmerius_chrysops",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  },
+  {
+    "scientificName": "Zonotrichia capensis",
+    "name": "Zonotrichia capensis",
+    "imageUrl": "",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Zonotrichia_capensis",
+    "family": "Unknown",
+    "conservationStatus": "LC"
+  }
+];
